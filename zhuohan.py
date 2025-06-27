a6a159fcf744f670a88333e4c3ef2e8bd94db004
zhuohany <zhuohanye@gmail.com>
2025-06-27 11:31:20 -0400

    Testing push

--- zhuohan.py
+++ zhuohan.py
@@ -1,2 +1,3 @@
 print("Hello World")
 print("This is Version 1.2 where I am still testing with out a pipeline, and also debugging some git issue.")
+print("Testing if git push creates a new docker image!")
